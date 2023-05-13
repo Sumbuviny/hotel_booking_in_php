9dfadb9d619d6cc1d83062813a33e76af71c385a
viny sumbu <Sumbuviny3@gmail.com>
2023-05-13 14:43:57 +0200

    update

--- hotel.php
+++ hotel.php
@@ -146,7 +146,7 @@
 
         <!--Section Services-->
 
-           <div class="services" id="service">
+        <div class="services" id="services">
                  <div class="container">
                         <div class="main">
                             <h1><span>S</span>ervices</h1>
@@ -216,7 +216,5 @@
                 </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-
     </body>
 </html>
